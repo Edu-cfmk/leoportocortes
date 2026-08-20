2df40b6d26f6a1b9dca956a5fdc064448ff3389a
Edu-cfmk <adu.carvalho321@gmail.com>
2026-08-19 21:20:30 -0300

    feat: adiciona icones e configuracao final do pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Léo Porto Cortes | Barbearia",
   description: "Sistema de agendamentos para a Barbearia Léo Porto Cortes",
-}
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icon-192x192.png",
+    apple: "/icon-192x192.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
